ae3fba5a298cd9d956e9bdf61938104d1e7bbb8f
Ivan <ivanolkhov@gmail.com>
2020-09-27 13:21:34 +0100

    final solution

--- src/main/scala/IntComp.scala
+++ src/main/scala/IntComp.scala
@@ -1,3 +1,14 @@
 object IntComp {
+  def integerComplexity(num: Int): Int = {
+    var result = num + 1
+    for (i <- 1 to num / 2) {
+      if (num % i == 0) {
+        val n = num / i
+        val m = n + i
+        if (m < result) result = m
+      }
+    }
+    result
+  }
 
 }
--- src/test/scala/IntCompTest.scala
+++ src/test/scala/IntCompTest.scala
@@ -1,5 +1,13 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 class IntCompTest extends AnyFunSuite {
-
+  test("12345") {
+    assert(IntComp.integerComplexity(12345) === 838)
+  }
+  test("12") {
+    assert(IntComp.integerComplexity(12) === 7)
+  }
+  test("4567") {
+    assert(IntComp.integerComplexity(4567) === 4568)
+  }
 }
